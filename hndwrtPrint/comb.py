69e0cacfc1e272118a0160abf863985b67f62dc3
Zhenduo Wang <wzdlc1996@gmail.com>
2021-07-20 22:40:21 +0800

    rewrite hndwrt

--- hndwrtPrint/comb.py
+++ hndwrtPrint/comb.py
@@ -36,21 +36,33 @@ class artic:
             lines += [list(para)[i:i+lineMaxCharNum] for i in range(0, len(para), lineMaxCharNum)]
         return lines
 
+    def splitPages(self, lineWid: int, lineNum: int, paraIndent=True) -> list:
+        lines = [list(self.main["title"]), [""]]
+        for x in self.main["paras"]:
+            if paraIndent:
+                para = "  " + x
+            else:
+                para = x
+
+            lines += [list(para)[i:i+lineWid] for i in range(0, len(para), lineWid)]
+        lines = [lines[i:i+lineNum] for i in range(0, len(lines), lineNum)]
+        return lines
+
 
 def charNumInEachLine(page_w, char_w, dw):
     return int((page_w + dw) / (char_w + dw))
 
 
-def genPage(lines, width, height, dw, dh, pool):
-    # generate the background
-    size = (width, height)
+def page2img(page, **kwargs):
+    size = (kwargs["w"], kwargs["h"])
+    dw, dh = kwargs["dw"], kwargs["dh"]
     img = PIL.Image.new("RGBA", size, "white")
 
     # positioning and paste char images
     x = 0
     y = 0
     w, h = gener.getSize()
-    for ln in lines:
+    for ln in page:
         for char in ln:
             a = gener(char)
             # use a as the mask to make it transparent!
@@ -60,13 +72,10 @@ def genPage(lines, width, height, dw, dh, pool):
         x = 0
         y += dh + h
 
-    img.save("page.png")
+    return img
 
 
-
-
-
-def artic2img(width, height, dw, dh):
+def artic2img(artic, width, height, dw, dh):
     art = artic()
     w, h = gener.getSize()
     lineMaxCharNum = charNumInEachLine(width, w, dw)
@@ -77,5 +86,24 @@ def artic2img(width, height, dw, dh):
 
 
 if __name__ == "__main__":
-    lines = artic2img(1000, 1600, 10, 10)
-    genPage(lines[:20], 1000, 1600, 10, 10, {})
+    # lines = artic2img(1000, 1600, 10, 10)
+    # genPage_toImg(lines, 1000, 1600, 10, 10, {})
+
+    import os
+
+    test_prefix = "./test"
+    try:
+        os.mkdir(test_prefix)
+    except FileExistsError:
+        print("Test folder exists!")
+        exit(-1)
+
+    art = artic()
+    pages = []
+    for pg in art.splitPages(16, 20):
+        pages.append(page2img(pg, w=1000, h=1600, dw=10, dh=10))
+
+    page_id = 1
+    for im in pages:
+        im.save(f"{test_prefix}/page_{page_id}.png")
+        page_id += 1
